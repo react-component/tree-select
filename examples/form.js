17d53413d1d0bcd2c0297b1cebd9a9ee19d7d40e
然则 <hualei.hl@alibaba-inc.com>
2016-02-01 15:53:44 +0800

    up form demo

--- examples/form.js
+++ examples/form.js
@@ -9,6 +9,21 @@ import {createForm} from 'rc-form';
 import {regionStyle, errorStyle} from './styles';
 import { gData } from './util';
 
+class TreeSelectInput extends Component {
+  onChange(value) {
+    console.log(value, arguments);
+    const props = this.props;
+    if (props.onChange) {
+      props.onChange(value);
+    }
+  }
+  render() {
+    return (
+      <TreeSelect {...this.props} onChange={this.onChange.bind(this)} />
+    );
+  }
+}
+
 @createForm()
 class Form extends Component {
   static propTypes = {
@@ -45,7 +60,7 @@ class Form extends Component {
       <form onSubmit={this.onSubmit}>
         <div style={regionStyle}>
           <div>
-            <p style={{color: 'blue'}}>work rightly</p>
+            <p style={{color: 'blue'}}>no onChange</p>
             <TreeSelect style={{width: 300}} {...tProps}
               {...getFieldProps('tree-select', {
                 initialValue: ['0-0-0-value'],
@@ -56,6 +71,22 @@ class Form extends Component {
           </div>
           <p style={errorStyle}>
             {(getFieldError('tree-select')) ? getFieldError('tree-select').join(',') : null}
+          </p>
+        </div>
+
+        <div style={regionStyle}>
+          <div>
+            <p style={{color: 'blue'}}>custom onChange</p>
+            <TreeSelectInput style={{width: 300}} {...tProps}
+              {...getFieldProps('tree-select1', {
+                initialValue: ['0-0-0-value'],
+                rules: [
+                  {required: true, type: 'array', message: 'tree-select1 需要必填'},
+                ],
+              })} />
+          </div>
+          <p style={errorStyle}>
+            {(getFieldError('tree-select1')) ? getFieldError('tree-select1').join(',') : null}
           </p>
         </div>
 
