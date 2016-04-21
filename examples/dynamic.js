82a9b3eb4519f693278aba40b508c8d2786d209a
然则 <hualei.hl@alibaba-inc.com>
2016-04-21 12:28:36 +0800

    Updates

--- examples/dynamic.js
+++ examples/dynamic.js
@@ -1,14 +1,14 @@
-webpackJsonp([1],{
+webpackJsonp([2],{
 
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(231);
+	module.exports = __webpack_require__(233);
 
 
 /***/ },
 
-/***/ 231:
+/***/ 233:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* eslint react/no-multi-comp:0, no-console:0 */
