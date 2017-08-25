8767ebd9cc8f935b3c3de171a80805f1fe1af65b
afc163 <afc163@gmail.com>
2017-08-25 17:16:03 +0800

    Fix check error: labs is undefined
    close ant-design/ant-design#7312

--- tests/Select.checkable.spec.js
+++ tests/Select.checkable.spec.js
@@ -53,4 +53,35 @@ describe('TreeSelect.checkable', () => {
     wrapper.find('.rc-tree-select-selection__clear').simulate('click');
     expect(wrapper.find('.rc-tree-select-selection__choice')).toHaveLength(0);
   });
+
+  // Fix https://github.com/ant-design/ant-design/issues/7312#issuecomment-324865971
+  it('should be checkable when treeCheckStrictly is true', () => {
+    const treeData = [
+      {label: '1-1', value: '1-1', children: []},
+      {label: '1-2', value: '1-2', children: []},
+      {label: '1-3', value: '1-3', children: [
+        {label: '2-1', value: '2-1', children: []},
+        {label: '2-2', value: '2-2', children: []},
+      ]},
+    ];
+    const handleChange = jest.fn();
+    const wrapper = mount(
+      <TreeSelect
+        treeData={treeData}
+        treeCheckable
+        treeCheckStrictly
+        multiple
+        onChange={handleChange}
+      />
+    );
+    // open
+    jest.useFakeTimers();
+    wrapper.find('.rc-tree-select').simulate('click');
+    jest.runAllTimers();
+    // select
+    wrapper.find('.rc-tree-select-tree-checkbox').at(0).simulate('click');
+    expect(handleChange).toBeCalled();
+    expect(wrapper.find('.rc-tree-select-selection__choice__content').length).toBe(1);
+    expect(wrapper.find('.rc-tree-select-selection__choice__content').at(0).text()).toBe('1-1');
+  });
 });
