6507b98e00b3eb1e6839caf858f6079fe80c609a
zombiej <smith3816@gmail.com>
2018-04-02 18:37:57 +0800

    input search check

--- src/Select.jsx
+++ src/Select.jsx
@@ -153,10 +153,7 @@ class Select extends Component {
       value = this.getValue(nextProps, value);
       this.setState({
         value,
-      }, () => {
-        // Refresh popup position
-        this.trigger.trigger.forcePopupAlign();
-      });
+      }, this.forcePopupAlign);
       // if (nextProps.combobox) {
       //   this.setState({
       //     inputValue: value.length ? String(value[0].key) : '',
@@ -213,7 +210,7 @@ class Select extends Component {
     this.setState({
       inputValue: val,
       open: true,
-    });
+    }, this.forcePopupAlign);
     if (props.treeCheckable && !val) {
       this.setState({
         value: this.getValue(props, [...this.state.value], false),
@@ -393,7 +390,7 @@ class Select extends Component {
   }
 
   onChoiceAnimationLeave = () => {
-    this.trigger.trigger.forcePopupAlign();
+    this.forcePopupAlign();
   }
 
   getLabelFromNode(child) {
@@ -771,10 +768,7 @@ class Select extends Component {
               label: labs && labs[i],
             };
           })),
-        }, () => {
-          // Refresh popup position
-          this.trigger.trigger.forcePopupAlign();
-        });
+        }, this.forcePopupAlign);
       }
     }
   }
@@ -802,6 +796,10 @@ class Select extends Component {
       this.getInputDOMNode().blur();
     }
   }
+
+  forcePopupAlign = () => {
+    this.trigger.trigger.forcePopupAlign();
+  };
 
   renderTopControlNode() {
     const { value } = this.state;
