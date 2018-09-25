ac16212d4e0bf7c6a7e9d511a421cbf3a4f02016
zombiej <smith3816@gmail.com>
2018-09-25 23:45:36 +0800

    add test case

--- tests/Select.tree.spec.js
+++ tests/Select.tree.spec.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-undef, react/no-multi-comp, no-console */
 import React from 'react';
-import { mount, render } from 'enzyme';
-import { renderToJson } from 'enzyme-to-json';
-import Tree, { TreeNode } from 'rc-tree';
-import Trigger from 'rc-trigger';
-import TreeSelect, { SHOW_ALL, SHOW_CHILD, SHOW_PARENT, TreeNode as SelectNode } from '../src';
+import { mount } from 'enzyme';
+import TreeSelect, { TreeNode as SelectNode } from '../src';
 import { resetAriaId } from '../src/util';
 import { setMock } from './__mocks__/rc-animate';
 
@@ -12,31 +9,72 @@ describe('TreeSelect.tree', () => {
   beforeEach(() => {
     jest.useFakeTimers();
     resetAriaId();
+    setMock(true);
   });
 
   afterEach(() => {
     jest.useRealTimers();
+    setMock(false);
   });
 
   const createSelect = (props) => (
-    <Tree {...props}>
-      <TreeNode key="0-0">
-        <TreeNode key="0-0-0">
-        <TreeNode key="0-0-0-0" />
-        <TreeNode key="0-0-0-1" />
-        </TreeNode>
-        <TreeNode key="0-0-1">
-          <TreeNode key="0-0-1-0" />
-          <TreeNode key="0-0-1-1" />
-        </TreeNode>
-      </TreeNode>
-    </Tree>
+    <TreeSelect {...props}>
+      <SelectNode key="0-0">
+        <SelectNode key="0-0-0">
+          <SelectNode key="0-0-0-0" />
+          <SelectNode key="0-0-0-1" />
+        </SelectNode>
+        <SelectNode key="0-0-1">
+          <SelectNode key="0-0-1-0" />
+          <SelectNode key="0-0-1-1" />
+        </SelectNode>
+      </SelectNode>
+    </TreeSelect>
   );
 
-  describe('treeExpandedKeys', () => {
-    it('controlled', () => {
-      // const 
-      // const wrapper = mount(createSelect({}));
-    });
+  it('treeExpandedKeys', () => {
+    class Test extends React.Component {
+      state = {
+        treeExpandedKeys: [],
+      };
+
+      onTreeExpand = (treeExpandedKeys) => {
+        this.setState({
+          treeExpandedKeys,
+        });
+      };
+
+      onReset = () => {
+        this.setState({
+          treeExpandedKeys: [],
+        });
+      };
+
+      render() {
+        return (
+          <div>
+            {createSelect({
+              open: true,
+              treeExpandedKeys: this.state.treeExpandedKeys,
+              onTreeExpand: this.onTreeExpand,
+            })}
+            <button onClick={this.onReset} className="reset">
+              Reset
+            </button>
+          </div>
+        );
+      }
+    }
+
+    const wrapper = mount(<Test />);
+
+    wrapper.find('.rc-tree-select-tree-switcher').simulate('click');
+    expect(wrapper.state().treeExpandedKeys).toEqual(['0-0']);
+
+    wrapper.find('.rc-tree-select-tree-switcher').at(2).simulate('click');
+    expect(wrapper.state().treeExpandedKeys).toEqual(['0-0', '0-0-1']);
+
+    wrapper.find('button.reset').simulate('click');
+    expect(wrapper.find('Tree').instance().state.expandedKeys).toEqual([]);
   });
 });
