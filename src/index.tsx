3b4bcaa1f4e8191da484163b79f8cf40a9873a2c
zombiej <smith3816@gmail.com>
2019-09-27 14:25:02 +0800

    chore: Export TreeSelectProps type

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import TreeSelect from './TreeSelect';
+import TreeSelect, { TreeSelectProps } from './TreeSelect';
 import TreeNode from './TreeNode';
 import { SHOW_ALL, SHOW_CHILD, SHOW_PARENT } from './utils/strategyUtil';
 
-export { TreeNode, SHOW_ALL, SHOW_CHILD, SHOW_PARENT };
+export { TreeNode, SHOW_ALL, SHOW_CHILD, SHOW_PARENT, TreeSelectProps };
 
 export default TreeSelect;
