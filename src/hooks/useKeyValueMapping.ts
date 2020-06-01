9b090d499fe60c2d3216f68cb79724583d1956f0
yoyo837 <yoyo837@hotmail.com>
2020-06-01 18:35:46 +0800

    恢复 isDisabled 说不定用得着，顺便看看codecov

--- src/hooks/useKeyValueMapping.ts
+++ src/hooks/useKeyValueMapping.ts
@@ -3,6 +3,23 @@ import { FlattenDataNode, Key, RawValueType } from '../interface';
 
 export type SkipType = null | 'select' | 'checkbox';
 
+export function isDisabled(dataNode: FlattenDataNode, skipType: SkipType): boolean {
+  if (!dataNode) {
+    return true;
+  }
+
+  const { disabled, disableCheckbox } = dataNode.data;
+
+  switch (skipType) {
+    case 'select':
+      return disabled;
+    case 'checkbox':
+      return disabled || disableCheckbox;
+  }
+
+  return false;
+}
+
 export default function useKeyValueMapping(
   cacheKeyMap: Map<Key, FlattenDataNode>,
   cacheValueMap: Map<RawValueType, FlattenDataNode>,
