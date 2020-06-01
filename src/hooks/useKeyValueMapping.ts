19d82033984ec82457c442ea2b252c18bbe97ca7
yoyo837 <yoyo837@hotmail.com>
2020-06-01 18:41:55 +0800

    Revert "恢复 isDisabled 说不定用得着，顺便看看codecov"
    This reverts commit 345a5ddfe1e0b538b2e8552f57e5646395d4efe2.

--- src/hooks/useKeyValueMapping.ts
+++ src/hooks/useKeyValueMapping.ts
@@ -3,23 +3,6 @@ import { FlattenDataNode, Key, RawValueType } from '../interface';
 
 export type SkipType = null | 'select' | 'checkbox';
 
-export function isDisabled(dataNode: FlattenDataNode, skipType: SkipType): boolean {
-  if (!dataNode) {
-    return true;
-  }
-
-  const { disabled, disableCheckbox } = dataNode.data;
-
-  switch (skipType) {
-    case 'select':
-      return disabled;
-    case 'checkbox':
-      return disabled || disableCheckbox;
-  }
-
-  return false;
-}
-
 export default function useKeyValueMapping(
   cacheKeyMap: Map<Key, FlattenDataNode>,
   cacheValueMap: Map<RawValueType, FlattenDataNode>,
