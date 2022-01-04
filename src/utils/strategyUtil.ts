e67be8073d325adca0772047d061e690d429d5d0
MadCcc <1075746765@qq.com>
2022-01-04 10:50:14 +0800

    fix: show selected parent whose children were all disabled

--- src/utils/strategyUtil.ts
+++ src/utils/strategyUtil.ts
@@ -25,9 +25,7 @@ export function formatStrategyValues(
       if (
         entity &&
         entity.children &&
-        entity.children.every(
-          ({ node }) => isCheckDisabled(node) || valueSet.has(node[fieldNames.value]),
-        )
+        entity.children.every(({ node }) => valueSet.has(node[fieldNames.value]))
       ) {
         return false;
       }
