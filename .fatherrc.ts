e9d2df7ecb74283f6eec7d3836f1b75f01ff3d4f
Peach <scdzwyxst@gmail.com>
2022-10-21 23:44:27 +0800

    build: correct alias config

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -5,7 +5,9 @@ export default defineConfig({
   cjs: { output: 'lib' },
   esm: {
     output: 'es',
-    alias: { 'rc-util/lib': 'rc-util/es' },
-    alias: { 'rc-tree/lib': 'rc-tree/es' },
+    alias: {
+      'rc-util/lib': 'rc-util/es',
+      'rc-tree/lib': 'rc-tree/es',
+    },
   },
 });
