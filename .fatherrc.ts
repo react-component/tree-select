0e1a007c9fcdb48098982937d2477cecf4c969ae
PeachScript <scdzwyxst@gmail.com>
2022-10-25 21:10:10 +0800

    build: use rc father plugin

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,13 +1,5 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
-  platform: 'browser',
-  cjs: { output: 'lib' },
-  esm: {
-    output: 'es',
-    alias: {
-      'rc-util/lib': 'rc-util/es',
-      'rc-tree/lib': 'rc-tree/es',
-    },
-  },
+  plugins: ['@rc-component/father-plugin'],
 });
